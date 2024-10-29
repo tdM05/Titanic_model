f80cdff1a5863d44e1407df02d36b7cf67cf049f
tdM05 <taddmao@gmail.com>
2024-10-29 13:54:55 -0400

    a

--- Model1_split.py
+++ Model1_split.py
@@ -1,21 +1,27 @@
 
-from train_model import mae_model, generate_csv
+from train_model import mae_model, generate_csv, generate_forest_csv, mae_forest_model
 
 features1 = ['Pclass', 'Age', 'Sex_indicator', 'Parch']
 features2 = ['Pclass', 'Age', 'Sex_indicator', 'Parch',
              'SibSp', 'Fare', 'Embarked_indicator']
-mae_model(features2, 4)
-generate_csv(features2)
-# features_list= [
-#
-# ]
-# min_mae = -1
-# min_nodes = -1
-# for i in range(2, 100):
-#     mae = mae_model(features, i)
-#     if min_mae == -1 or min_mae < mae:
-#         min_mae = mae
-#         min_nodes = i
-# print(f"min_nodes:{min_nodes}, min_mae:{min_mae}")
+features3 = ['Pclass', 'Age', 'Sex_indicator', 'Parch',
+             'SibSp', 'Fare']
+features4 = ['Pclass', 'Age', 'Sex_indicator', 'Parch',
+             'SibSp']
+features5 = ['Pclass', 'Age', 'Sex_indicator', 'Parch',
+             'Fare']
+features_list = [features1, features2, features3, features4, features5]
+print(mae_forest_model(features3, 6))
 
+# for features in features_list:
+#     min_mae = -1
+#     min_nodes = -1
+#     for i in range(2, 100):
+#         mae = mae_model(features, i)
+#         if min_mae == -1 or min_mae > mae:
+#             min_mae = mae
+#             min_nodes = i
+#     print(f"min_nodes:{min_nodes}, min_mae:{min_mae}")
+
+# min_nodes 6 is optimal for features 3
 # generate_csv(features, 4)
--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
@@ -56,3 +57,54 @@ def generate_csv(features: list[str]):
     output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
     output.to_csv('my_submission.csv', index=False)
 
+def mae_forest_model(features: list[str], max_leaf_nodes: int) -> None:
+    """Prints the mae"""
+    train_path = "./resources/train.csv"
+    # Load data
+    data = pd.read_csv(train_path)
+    # convert sex feature into numerical values
+    data['Sex_indicator'] = data['Sex'].map({'male': 0, 'female': 1})
+    data['Embarked_indicator'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+    X = data[features]
+    y = data.Survived
+
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+    # Define model
+    model = RandomForestClassifier(
+        max_leaf_nodes=max_leaf_nodes,
+        random_state=1
+    )
+    model.fit(train_X, train_y)
+    # print(type(data.PassengerId))
+    # Get predictions
+    predictions = model.predict(val_X)
+    mae = mean_absolute_error(predictions, val_y)
+    # print("Mean Absolute Error: ", mae)
+    return mae
+    # output = pd.DataFrame({'PassengerId': data.PassengerId, 'Survived': predictions})
+    # output.to_csv('splitted.csv', index=False)
+
+def generate_forest_csv(features: list[str], max_leaf_nodes: int):
+    """Prints the mae"""
+    train_path = "./resources/train.csv"
+
+    # Load data
+    data = pd.read_csv(train_path)
+    # convert sex feature into numerical values
+    data['Sex_indicator'] = data['Sex'].map({'male': 0, 'female': 1})
+    data['Embarked_indicator'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+    X = data[features]
+    y = data.Survived
+    # Define model
+    model = RandomForestClassifier(max_leaf_nodes=max_leaf_nodes, random_state=1)
+    model.fit(X, y)
+
+    # Get predictions
+    test_path = "./resources/test.csv"
+    test_data = pd.read_csv(test_path)
+    test_data['Sex_indicator'] = test_data['Sex'].map({'male': 0, 'female': 1})
+    test_data['Embarked_indicator'] = test_data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+    predictions = model.predict(test_data[features])
+    output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+    output.to_csv('my_submission.csv', index=False)
