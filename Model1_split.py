beaf01d98fdc6560a529796720300bbc0a2fdea1
tdM05 <taddmao@gmail.com>
2024-10-29 13:32:37 -0400

    s

--- Model1_split.py
+++ Model1_split.py
@@ -1,6 +1,21 @@
 
 from train_model import mae_model, generate_csv
 
-features = ['Pclass', 'Age', 'Sex_indicator', 'Parch']
-mae_model(features)
-# generate_csv(features)
+features1 = ['Pclass', 'Age', 'Sex_indicator', 'Parch']
+features2 = ['Pclass', 'Age', 'Sex_indicator', 'Parch',
+             'SibSp', 'Fare', 'Embarked_indicator']
+mae_model(features2, 4)
+generate_csv(features2)
+# features_list= [
+#
+# ]
+# min_mae = -1
+# min_nodes = -1
+# for i in range(2, 100):
+#     mae = mae_model(features, i)
+#     if min_mae == -1 or min_mae < mae:
+#         min_mae = mae
+#         min_nodes = i
+# print(f"min_nodes:{min_nodes}, min_mae:{min_mae}")
+
+# generate_csv(features, 4)
--- train_model.py
+++ train_model.py
@@ -1,30 +1,34 @@
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 
 
-def mae_model(features: list[str]) -> None:
+def mae_model(features: list[str], max_leaf_nodes: int) -> None:
     """Prints the mae"""
     train_path = "./resources/train.csv"
     # Load data
     data = pd.read_csv(train_path)
     # convert sex feature into numerical values
     data['Sex_indicator'] = data['Sex'].map({'male': 0, 'female': 1})
+    data['Embarked_indicator'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
     X = data[features]
     y = data.Survived
 
     train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
     # Define model
-    model = DecisionTreeRegressor(random_state=1)
+    model = DecisionTreeClassifier(
+        max_leaf_nodes=max_leaf_nodes,
+        random_state=1
+    )
     model.fit(train_X, train_y)
-
+    # print(type(data.PassengerId))
     # Get predictions
     predictions = model.predict(val_X)
     mae = mean_absolute_error(predictions, val_y)
-    print("Mean Absolute Error: ", mae)
-
+    # print("Mean Absolute Error: ", mae)
+    return mae
     # output = pd.DataFrame({'PassengerId': data.PassengerId, 'Survived': predictions})
     # output.to_csv('splitted.csv', index=False)
 
@@ -36,16 +40,18 @@ def generate_csv(features: list[str]):
     data = pd.read_csv(train_path)
     # convert sex feature into numerical values
     data['Sex_indicator'] = data['Sex'].map({'male': 0, 'female': 1})
+    data['Embarked_indicator'] = data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
     X = data[features]
     y = data.Survived
     # Define model
-    model = DecisionTreeRegressor(random_state=1)
+    model = DecisionTreeClassifier(random_state=1)
     model.fit(X, y)
 
     # Get predictions
     test_path = "./resources/test.csv"
     test_data = pd.read_csv(test_path)
     test_data['Sex_indicator'] = test_data['Sex'].map({'male': 0, 'female': 1})
+    test_data['Embarked_indicator'] = test_data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
     predictions = model.predict(test_data[features])
     output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
     output.to_csv('my_submission.csv', index=False)
